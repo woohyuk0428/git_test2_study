7e9480d419c184fb6c8280d935ef92029e79403c
woohyuk0428 <whkang03@gmail.com>
2026-04-01 00:11:33 +0900

    최초 커밋

---
+++ src/git_test2_study/Hello.java
@@ -0,0 +1,8 @@
+package git_test2_study;
+
+public class Hello {
+
+	public void say() {
+		System.out.println("hello world");
+	}
+}
---
+++ src/git_test2_study/Main.java
@@ -0,0 +1,8 @@
+package git_test2_study;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Hello().say();
+	}
+}
